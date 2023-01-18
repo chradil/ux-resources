fc063d119737c9ab2297361bff0580063af7683a
chradil <cassandra.hearsay@gmail.com>
2023-01-18 13:56:38 -0500

    fix baseurls (pathPrefix)

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,10 @@
-const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
-
 module.exports = function(eleventyConfig) {
-
-  eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
   // Output directory: _site
 
   // Copy `img/` to `_site/img`
   eleventyConfig.addPassthroughCopy("assets");
+
+  return {
+    pathPrefix: "/eleventy-base-blog/"
+  }
 }
