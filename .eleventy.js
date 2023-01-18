3f2935d1fa2976872642b6fcb0b7090162ccd1b4
chradil <cassandra.hearsay@gmail.com>
2023-01-18 13:53:06 -0500

    fix baseurls

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,8 @@
+const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
+
 module.exports = function(eleventyConfig) {
+
+  eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
   // Output directory: _site
 
   // Copy `img/` to `_site/img`
