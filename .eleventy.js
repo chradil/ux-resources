159c0f92e31776d5d6369234377e3160704c4f15
chradil <cassandra.hearsay@gmail.com>
2023-01-18 14:00:22 -0500

    fix urls

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,12 @@
 module.exports = function(eleventyConfig) {
+  return {
+    pathPrefix: "/ux-resources/"
+  }
+
   // Output directory: _site
 
   // Copy `img/` to `_site/img`
   eleventyConfig.addPassthroughCopy("assets");
 
-  return {
-    pathPrefix: "/eleventy-base-blog/"
-  }
+
 }
