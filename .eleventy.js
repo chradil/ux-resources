4e536b15b1c55109e65c72acfc6dd503b658e103
chradil <cassandra.hearsay@gmail.com>
2023-01-19 14:49:52 -0500

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,12 @@
 module.exports = function(eleventyConfig) {
-  return {
-    pathPrefix: "/ux-resources/"
-  }
-
+  
   // Output directory: _site
 
   // Copy `img/` to `_site/img`
   eleventyConfig.addPassthroughCopy("assets");
 
+  return {
+      pathPrefix: "/ux-resources/"
+    }
 
 }
